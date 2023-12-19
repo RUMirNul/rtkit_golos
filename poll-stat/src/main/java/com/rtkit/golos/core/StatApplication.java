3e72ae834396a7b4b537213943ab5c0c495c3d7d
Anthony Makushkin <anthony.makushkin@yahoo.com>
2023-12-20 00:52:09 +0300

    Добавил модули для ядра, рассылки и статистики

---
+++ poll-stat/src/main/java/com/rtkit/golos/core/StatApplication.java
@@ -0,0 +1,13 @@
+package com.rtkit.golos.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatApplication.class, args);
+	}
+
+}
